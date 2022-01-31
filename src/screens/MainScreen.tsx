9034c10e2b52e07d72f7e7aa56f51ad4570707d8
NenadoPL1Z <limon4ikpro1@gmail.com>
2022-01-31 17:58:51 +0500

    BottomTab

--- src/screens/MainScreen.tsx
+++ src/screens/MainScreen.tsx
@@ -1,13 +1,15 @@
-import React from 'react';
+import React, { FC } from 'react';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import TestComp from '../components/TestComp';
 import MessageScreen from './MessageScreen';
 import PlaceOfferScreen from './PlaceOfferScreen';
 import FavoriteScreen from './FavoriteScreen';
 import AccountScreen from './AccountScreen';
-// @ts-ignore
-import {ReactComponent as Logo} from '../assets/LogoIcon.svg';
-import { Text } from 'react-native-elements';
+import Logo from '../assets/LogoIcon.svg';
+import Message from '../assets/Message.svg';
+import Like from '../assets/FavoriteLike.svg';
+import Profile from '../assets/AccountProfile.svg';
+import PlaceOffer from '../assets/AddNewProductIcon.svg';
 
 const buttonsNavigation = [
   {name: 'MainScreen', component: TestComp, options: {title: 'Поиск'}},
@@ -33,22 +35,35 @@ const buttonsNavigation = [
   },
 ];
 
-const MainScreen = () => {
+const generateIcon = (routeName: string, focused: boolean, color: string) => {
+  const styleColor = {color: focused ? color : '#8F8F8F'};
+  switch (routeName) {
+    case 'MainScreen':
+      return <Logo style={styleColor as {}} />;
+    case 'PlaceOfferScreen':
+      return <PlaceOffer style={styleColor as {}} />;
+    case 'FavoriteScreen':
+      return <Like style={styleColor as {}} />;
+    case 'AccountScreen':
+      return <Profile style={styleColor as {}} />;
+    case 'MessageScreen':
+      return <Message style={styleColor as {}} />;
+  }
+};
+
+const MainScreen: FC = () => {
   const Tab = createBottomTabNavigator();
 
   return (
-    <Tab.Navigator>
+    <Tab.Navigator
+      screenOptions={({route}) => ({
+        tabBarIcon: ({focused, color}) => {
+          return generateIcon(route.name, focused, color);
+        },
+        tabBarActiveTintColor: '#00A0AB',
+      })}>
       {buttonsNavigation.map(buttonProps => (
-        <Tab.Screen
-          key={buttonProps.name}
-          {...buttonProps}
-          options={{
-            title: buttonProps.options.title,
-            tabBarIcon: ({color, size}) => {
-              return <Text>23</Text>;
-            },
-          }}
-        />
+        <Tab.Screen key={buttonProps.name} {...buttonProps} />
       ))}
     </Tab.Navigator>
   );
