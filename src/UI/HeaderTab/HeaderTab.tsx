cc7adb37a871f0c210e2bce8eeabb4b24c65afd4
NenadoPL1Z <limon4ikpro1@gmail.com>
2022-03-03 17:49:23 +0500

    change

--- src/UI/HeaderTab/HeaderTab.tsx
+++ src/UI/HeaderTab/HeaderTab.tsx
@@ -2,14 +2,15 @@ import React, { FC } from 'react';
 import { View, Pressable } from 'react-native';
 import RobotoText from '../RobotoText';
 import ArrowBack from '../../assets/ArrowLeft.svg';
-import { HeaderTabStyles } from './styles';
+import { useHeaderTabStyles } from './styles';
 import { useRouter } from '../../hooks/useRouter';
 import { HeaderTabProps } from './types';
 import SaveAreaTheme from '../SaveAreaTheme/SaveAreaTheme';
 
-const HeaderTab: FC<HeaderTabProps> = ({ title }) => {
-  const styles = HeaderTabStyles();
+const HeaderTab: FC<HeaderTabProps> = ({ title, rightContent = null }) => {
+  const styles = useHeaderTabStyles();
   const { pushBack } = useRouter();
+  const RightComponent = rightContent;
 
   return (
     <SaveAreaTheme>
@@ -19,9 +20,16 @@ const HeaderTab: FC<HeaderTabProps> = ({ title }) => {
             <ArrowBack />
           </View>
         </Pressable>
-        <RobotoText weight="b" style={styles.title}>
-          {title}
-        </RobotoText>
+        <View style={styles.content}>
+          <RobotoText weight="b" style={styles.title}>
+            {title}
+          </RobotoText>
+          {RightComponent && (
+            <View style={styles.rightContent}>
+              <RightComponent />
+            </View>
+          )}
+        </View>
       </View>
     </SaveAreaTheme>
   );
--- src/components/ScrollCardData/ScrollCardData.tsx
+++ src/components/ScrollCardData/ScrollCardData.tsx
@@ -1,13 +1,17 @@
 import React from 'react';
-import { View } from 'react-native';
+import { Pressable, View } from 'react-native';
 import { Text } from 'react-native-elements';
 import { useSize } from '../../hooks/useSize';
+import { useRouter } from '../../hooks/useRouter';
 
 const ScrollCardData = () => {
   const { deviceHeight } = useSize();
+  const { pushTo } = useRouter();
   return (
     <View style={{ position: 'relative', zIndex: -1, height: deviceHeight }}>
-      <Text style={{ color: 'red' }}>CARD</Text>
+      <Pressable onPress={pushTo('ProductScreen')}>
+        <Text style={{ color: 'red' }}>CARD</Text>
+      </Pressable>
     </View>
   );
 };
