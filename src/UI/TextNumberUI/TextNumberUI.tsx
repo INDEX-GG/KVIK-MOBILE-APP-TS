8755a99610ea362617eb1fdc404da87a685a03bd
NenadoPL1Z <limon4ikpro1@gmail.com>
2022-05-26 10:53:42 +0500

    placeOffer-changes

--- src/UI/TextNumberUI/TextNumberUI.tsx
+++ src/UI/TextNumberUI/TextNumberUI.tsx
@@ -3,41 +3,56 @@ import { TextInput, View } from 'react-native';
 import {
   ITextNumberUIProps,
 } from '../../models/IAdditionalFieldsModel';
-import { useTextUIStyles } from './../TextUI/style';
+import { useTextNumberUIStyles } from './style';
 import { useTextNumber } from './useTextNumberUI';
 import { Controller } from 'react-hook-form';
-import UbuntuTextUI from '../UbuntuTextUI/UbuntuTextUI';
+import RobotoText from '../RobotoText';
 
 const TextNumberUI: FC<ITextNumberUIProps> = (props) => {
-  const styles = useTextUIStyles();
-  const {alias, default_value} = props;
+  const styles = useTextNumberUIStyles();
+  const {
+    alias,
+    title,
+    number_version,
+    default_value,
+    number_max_value,
+    // number_unit_of_measure,
+  } = props;
 
-  const { control, handleChangeText, placeholderTitle } = useTextNumber(props);
-  console.log(123);
+  const {
+    control,
+    handleChangeText,
+    // generateAfterString
+  } = useTextNumber(number_version);
 
   return (
-    <Controller
-      name={alias}
-      control={control}
-      defaultValue={default_value}
-      render={({field: {value, onChange}} ) => (
-        <View style={styles.container}>
-          {value ? (
-            <UbuntuTextUI fontWeight={400} textProps={{style: styles.label}} >
-              {placeholderTitle}
-            </UbuntuTextUI>
-          ) : null}
-          <TextInput
-            value={value}
-            keyboardType="numeric"
-            onChangeText={(text) => handleChangeText(text, onChange)}
-            placeholder={placeholderTitle}
-            placeholderTextColor={styles.inputColor.color}
-            style={styles.inputContainer}
-          />
-        </View>
-      )}
-    />
+    <View style={styles.container}>
+      <Controller
+        name={alias}
+        control={control}
+        defaultValue={default_value}
+        render={({field: {value, onChange}} ) => (
+          <>
+            <TextInput
+              value={value}
+              maxLength={number_max_value}
+              keyboardType="numeric"
+              onChangeText={(text) => handleChangeText(text, onChange)}
+              placeholder={title}
+              placeholderTextColor={styles.inputColor.color}
+              style={styles.inputContainer}
+            />
+            {/*{value ? (*/}
+            {/*  <View style={{...styles.placeholderContainer, left: generateAfterString(value?.length)}}>*/}
+            {/*    <RobotoText weight="m" style={styles.inputPlaceholder}>*/}
+            {/*      {number_unit_of_measure}*/}
+            {/*    </RobotoText>*/}
+            {/*  </View>*/}
+            {/*) : null}*/}
+          </>
+        )}
+      />
+    </View>
   );
 };
 
